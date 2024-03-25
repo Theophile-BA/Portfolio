f55c5bd888ac5c823dcc11872d66cbd737a75e97
Theophile-BA <contact.theophileboniface@gmail.com>
2024-03-25 16:20:58 +0100

    🚀 First commit 🚀

---
+++ src/font/font.ts
@@ -0,0 +1,13 @@
+import { Inter, Montserrat, Roboto } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+
+export const montserrat = Montserrat({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+})
+
+export const roboto = Roboto({
+    weight: ['400', '400'],
+    subsets: ['latin'],
+})
